7f791d98ef55e69c86c571e70401bd56e0c34259
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2024-12-19 13:36:08 +0100

    try avg

--- stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/Average.java
+++ stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/Average.java
@@ -1,15 +1,21 @@
 package at.ac.uibk.dps.streamprocessingapplications.stats.transforms;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
 import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Instant;
 
 public class Average<T, Double> extends PTransform<PCollection<T>, PCollection<Double>> {
   private DoFn<Iterable<T>, Double> averagingFunction;
   private int batchSize;
+  private int currentSize;
 
   public Average(DoFn<Iterable<T>, Double> averagingFunction, int batchSize) {
     this.averagingFunction = averagingFunction;
     this.batchSize = batchSize;
+    this.currentSize = 0;
   }
 
   @Override
@@ -18,10 +24,36 @@ public class Average<T, Double> extends PTransform<PCollection<T>, PCollection<D
      * `GroupIntoBatches` only supports grouping for key-value pairs.
      * Therefore, a pseudo mapping to the same key is performed.
      */
+
     return input
-        .apply(WithKeys.of(""))
-        .apply(GroupIntoBatches.ofSize(this.batchSize))
-        .apply(Values.create())
-        .apply(ParDo.of(this.averagingFunction));
+        .apply(
+            "BufferElements",
+            ParDo.of(
+                new DoFn<T, List<T>>() {
+                  private final List<T> buffer = new ArrayList<>();
+                  private final Object lock = new Object();
+
+                  @ProcessElement
+                  public void processElement(ProcessContext c) {
+                    synchronized (lock) {
+                      buffer.add(c.element()); // Add element to the buffer
+                      if (buffer.size() >= batchSize) {
+                        c.output(new ArrayList<>(buffer)); // Output a batch as a new list
+                        buffer.clear(); // Clear the buffer
+                      }
+                    }
+                  }
+
+                  @FinishBundle
+                  public void finishBundle(FinishBundleContext c) {
+                    synchronized (lock) {
+                      if (!buffer.isEmpty()) {
+                        c.output(new ArrayList<>(buffer), Instant.now(), GlobalWindow.INSTANCE);
+                        buffer.clear();
+                      }
+                    }
+                  }
+                }))
+        .apply("AverageElements", ParDo.of(averagingFunction));
   }
 }
