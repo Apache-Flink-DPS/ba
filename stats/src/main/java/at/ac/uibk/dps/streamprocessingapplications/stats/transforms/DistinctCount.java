59cbdd1a730d80d8414ba17582c46df976e8cad6
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2024-12-19 15:37:47 +0100

    a

--- stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/DistinctCount.java
+++ stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/DistinctCount.java
@@ -1,6 +1,7 @@
 package at.ac.uibk.dps.streamprocessingapplications.stats.transforms;
 
 import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 
 public class DistinctCount<T> extends PTransform<PCollection<T>, PCollection<Long>> {
@@ -19,7 +20,19 @@ public class DistinctCount<T> extends PTransform<PCollection<T>, PCollection<Lon
      * Therefore, a pseudo mapping to the same key is performed.
      */
     return input
-        .apply(WithKeys.of(""))
+        .apply(
+            "AddKey",
+            ParDo.of(
+                new DoFn<T, KV<String, T>>() {
+                  private int count = 0;
+
+                  @ProcessElement
+                  public void processElement(ProcessContext c) {
+                    int key = count % 2;
+                    c.output(KV.of(String.valueOf(key), c.element()));
+                    count++;
+                  }
+                }))
         .apply(GroupIntoBatches.ofSize(this.batchSize))
         .apply(Values.create())
         .apply(ParDo.of(this.distinctCountFunction));
