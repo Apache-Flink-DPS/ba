b35fc7d79907fccf36dea1236d2de6f084b57c86
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2024-12-23 09:04:43 +0100

    added filter 5

--- stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/KalmanFilterFunction.java
+++ stats/src/main/java/at/ac/uibk/dps/streamprocessingapplications/stats/transforms/KalmanFilterFunction.java
@@ -14,37 +14,44 @@ public class KalmanFilterFunction<T> extends DoFn<T, T> {
   private SerializableFunction<T, Double> getter;
   private SerializableBiFunction<T, Double, T> setter;
 
+  private int counter;
+
   public KalmanFilterFunction(
       SerializableFunction<T, Double> getter, SerializableBiFunction<T, Double, T> setter) {
     this.getter = getter;
     this.setter = setter;
+    this.counter = 0;
   }
 
   @ProcessElement
   public void processElement(@Element T element, OutputReceiver<T> out) {
-    final double z_measuredValue = this.getter.apply(element);
+    if(counter == 5) {
+      final double z_measuredValue = this.getter.apply(element);
 
-    // NOTE: conditional override of the values due to `NullPointerException`
-    double x0_previousEstimation = 0.0;
-    try {
-      x0_previousEstimation = 0.0;
-    } catch (NullPointerException ignore) {
+      // NOTE: conditional override of the values due to `NullPointerException`
+      double x0_previousEstimation = 0.0;
+      try {
+        x0_previousEstimation = 0.0;
+      } catch (NullPointerException ignore) {
+      }
+
+      double p0_priorErrorCovariance = 0.0;
+      try {
+        p0_priorErrorCovariance = 0.0;
+      } catch (NullPointerException ignore) {
+      }
+
+      // INFO: The following code was copied from the original `riot-bench` implementation.
+      double p1_currentErrorCovariance = p0_priorErrorCovariance + q_processNoise;
+
+      double k_kalmanGain = p1_currentErrorCovariance / (p1_currentErrorCovariance + r_sensorNoise);
+      double x1_currentEstimation =
+          x0_previousEstimation + k_kalmanGain * (z_measuredValue - x0_previousEstimation);
+      p1_currentErrorCovariance = (1 - k_kalmanGain) * p1_currentErrorCovariance;
+
+      out.output(this.setter.apply(element, x1_currentEstimation));
+      counter = 0;
     }
-
-    double p0_priorErrorCovariance = 0.0;
-    try {
-      p0_priorErrorCovariance = 0.0;
-    } catch (NullPointerException ignore) {
-    }
-
-    // INFO: The following code was copied from the original `riot-bench` implementation.
-    double p1_currentErrorCovariance = p0_priorErrorCovariance + q_processNoise;
-
-    double k_kalmanGain = p1_currentErrorCovariance / (p1_currentErrorCovariance + r_sensorNoise);
-    double x1_currentEstimation =
-        x0_previousEstimation + k_kalmanGain * (z_measuredValue - x0_previousEstimation);
-    p1_currentErrorCovariance = (1 - k_kalmanGain) * p1_currentErrorCovariance;
-
-    out.output(this.setter.apply(element, x1_currentEstimation));
+    counter++;
   }
 }
