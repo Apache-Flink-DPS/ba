b50aab3a7cbbd47d8e5b72893af33fd581e6bc2a
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2025-01-07 13:44:30 +0100

    try

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/fit/RangeFilterFunction.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/fit/RangeFilterFunction.java
@@ -11,6 +11,8 @@ public class RangeFilterFunction
     implements SerializableFunction<FitnessMeasurements, FitnessMeasurements> {
 
   public FitnessMeasurements apply(FitnessMeasurements measurements) {
+    System.out.println(
+        "MEASUREMENTNTNTNNTNTNTNNTN--------------------------------_" + measurements);
     setNullIf(
         measurements::getAnkleAccelerationX,
         RangeFilterFunction::isAnkleAccelerationXOutOfRange,
@@ -37,14 +39,14 @@ public class RangeFilterFunction
    */
   private static <T> void setNullIf(
       Supplier<Optional<T>> getter, Predicate<T> condition, Consumer<T> setter) {
-      getter
-          .get()
-          .ifPresent(
-              value -> {
-                if (condition.test(value)) {
-                  setter.accept(null);
-                }
-              });
+    getter
+        .get()
+        .ifPresent(
+            value -> {
+              if (condition.test(value)) {
+                setter.accept(null);
+              }
+            });
   }
 
   private static boolean isAnkleAccelerationXOutOfRange(Double acceleration) {
