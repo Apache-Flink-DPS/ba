82205fc43be0e0aa38ec5ee459b0239966c89f43
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2025-01-20 11:13:22 +0100

    added

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/PipelineBuilder.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/PipelineBuilder.java
@@ -28,7 +28,7 @@ public class PipelineBuilder {
 
     PCollection<String> etl_strings =
         pipeline
-            .apply(new ReadSenMLSource("senml-source"))
+            .apply(new ReadSenMLSource("senml-cleaned"))
             .apply(
                 "Loader",
                 ParDo.of(
